497c1d37d14b92ef5d74b91f658048c6a6c4ee1f
Craig J Perry <craigp84@gmail.com>
2014-04-15 22:56:08 +0100

    Added given test case

--- test_euler.py
+++ test_euler.py
@@ -4,7 +4,7 @@
 
 import unittest
 import itertools
-from euler import multiples_of_3_and_5, fibonacci
+from euler import multiples_of_3_and_5, fibonacci, prime_factors
 
 
 class Problem1(unittest.TestCase):
@@ -20,6 +20,13 @@ class Problem2(unittest.TestCase):
     def test_given_example_first_ten_terms(self):
         terms = tuple(itertools.islice(fibonacci(), 10))
         self.assertTupleEqual(terms, (1, 2, 3, 5, 8, 13, 21, 34, 55, 89))
+
+
+class Problem3(unittest.TestCase):
+
+    def test_given_example_factors(self):
+        factors = tuple(prime_factors(13195))
+        self.assertTupleEqual(factors, (5, 7, 13, 29))
 
 
 if __name__ == "__main__":
