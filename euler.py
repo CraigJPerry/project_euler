6c4224098f942ecafed6fea65c1a77f9d18d7198
Craig J Perry <craigp84@gmail.com>
2014-04-15 22:45:26 +0100

    Satisfied test case and generated answer

--- euler.py
+++ euler.py
@@ -2,12 +2,25 @@
 # encoding: utf-8
 
 
+import itertools
+
+
 def multiples_of_3_and_5(until):
     return (x for x in xrange(1, until) if x % 3 == 0 or x % 5 == 0)
 
 
+def fibonacci(a=1, b=2):
+    """Infinite Fibonacci sequence generator."""
+    while True:
+        yield a
+        a, b = b, b+a
+
+
 def main():
     print "Problem 1: %d" % sum(multiples_of_3_and_5(1000))
+
+    problem2 = itertools.takewhile(lambda x: x<4000000, fibonacci())
+    print "Problem 2: %d" % sum(even for even in problem2 if even % 2 == 0)
 
 
 if __name__ == "__main__":
