df95481bfe8f0be5d1ef4e43cf8b5683760c9cee
Craig J Perry <craigp84@gmail.com>
2014-04-16 22:48:33 +0100

    Minor refactoring for readability. Expose problem solution via stdout.

--- euler/functional_style.py
+++ euler/functional_style.py
@@ -63,8 +63,11 @@ def smallest_evenly_divisible(start, end):
 
 
 def sum_square_difference(upper_limit):
-    sum_of_squares = reduce(lambda x, y: x + y*y, range(upper_limit + 1))
-    square_of_sum = sum(range(upper_limit + 1)) ** 2
+    """Problem 6: Find the difference of sum of squares and square of
+    sum for natural numbers to upper_limit."""
+    naturals = range(upper_limit + 1)
+    sum_of_squares = reduce(lambda x, y: x + y*y, naturals)
+    square_of_sum = sum(naturals) ** 2
     return square_of_sum - sum_of_squares
 
 
@@ -103,6 +106,9 @@ def main():
 
     problem5 = smallest_evenly_divisible(1, 20)
     print "Problem 5: %d" % problem5
+
+    problem6 = sum_square_difference(100)
+    print "Problem 6: %d" % problem6
 
 
 if __name__ == "__main__":
